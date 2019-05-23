b2bae512e1460e014b333cc43530f8f7698b23fc
doesthisusername <yfirestorm@gmail.com>
2019-05-23 13:57:51 +0200

    Refactor to allow playing multiple times during a single execution, easing workflow.
    Use with the -r switch.

--- hatTAS/hatTAS.cpp
+++ hatTAS/hatTAS.cpp
@@ -9,20 +9,20 @@
 #define HAT_EXE_NAME L"HatinTimeGame.exe"
 #define XINPUT_DLL_NAME L"XINPUT1_3.dll"
 #define MSVCR_DLL_NAME L"MSVCR100.dll"
-// divide by 4 because of pointer arithmetic
-#define GET_STATE_CODE_OFFSET (0x9AC3EA) // except this one because it's for a breakpoint
-#define POST_GET_STATE_CODE_OFFSET (0x9AC3EF) // this one too
-#define TICK_CODE_OFFSET (0x3D7A70 / 4) // maybe? need to test
-#define DELTA_WRITE_CODE_OFFSET (0x9D7F86 / 4)
-#define DELTA_DATA_OFFSET (0x104D6E8 / 4)
-#define TOTAL_FRAMES_OFFSET (0x11D07A0 / 4)
-#define TIMER_OFFSET (0x10719D0 / 4)
-#define FPS_PTR_OFFSET (0x11C27E0 / 4)
+
+#define GET_STATE_CODE_OFFSET (0x9AC3EA)
+#define POST_GET_STATE_CODE_OFFSET (0x9AC3EF)
+#define TICK_CODE_OFFSET (0x3D7A70)
+#define DELTA_WRITE_CODE_OFFSET (0x9D7F86)
+#define DELTA_DATA_OFFSET (0x104D6E8)
+#define TOTAL_FRAMES_OFFSET (0x11D07A0)
+#define TIMER_OFFSET (0x10719D0)
+#define FPS_PTR_OFFSET (0x11C27E0)
 // msvcr offsets
 #define RAND_CODE_OFFSET (0x7947D)
 
 // it only wants to write on 4-byte aligned boundaries, so the first two bytes are filler, as our target code isn't 4-byte aligned
-const BYTE nop_delta_buf[10] = { 0x7D, 0x00, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+const BYTE nop_delta_buf[8] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
 double desired_framerate = 1.0 / 60.0; // desired delta time
 float original_fps = 60.f; // fps setting before the TAS started
 double original_delta = 1.0 / 60.0; // for restoring framerate properly
@@ -42,9 +42,21 @@ const int zero = 0;
 // movie
 tas_metadata meta;
 input_report* reports;
+char* movie_filename = NULL;
 
+BYTE* base_address = NULL;
+BYTE* xinput_address = NULL;
+BYTE* msvcr_address = NULL;
+
+BYTE* fps_address = NULL;
+void* new_rand = NULL;
+unsigned long old_rand_prot;
+
+HWND window;
 HANDLE process;
 HANDLE thread;
+DWORD pid;
+DWORD tid;
 
 // only call after process handle has been made
 BYTE* resolve_ptr(void* address) {
@@ -53,59 +65,44 @@ BYTE* resolve_ptr(void* address) {
 	return resolved;
 }
 
-int main(int argc, char* argv[]) {
-	if(argc < 2) {
-		_tprintf(L"A TAS movie filename wasn't specified!\nExiting...\n");
-		return 0;
-	}
-
-	_tprintf(L"Parsing file...\n");
-	reports = parse_tas(argv[1], &meta);
-	_tprintf(L"Parsed file!\n");
-	desired_framerate = 1.f / meta.fps;
-
+int init() {
 	// find pid
-	HWND window = FindWindow(HAT_WINDOW, HAT_TITLE);
-	DWORD pid = 0;
-	DWORD tid = GetWindowThreadProcessId(window, &pid);
+	window = FindWindow(HAT_WINDOW, HAT_TITLE);
+	tid = GetWindowThreadProcessId(window, &pid);
 
 	// get process handle passing in the pid
 	process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 	if(process == NULL) {
-		_tprintf(L"Failed to open the HatinTimeGame process!\nExiting...\n");
+		_tprintf(L"Failed to open the HatinTimeGame process!\n");
 		return 0;
 	}
 	// get thread handle passing in the tid
 	thread = OpenThread(THREAD_ALL_ACCESS, FALSE, tid);
 	if(thread == NULL) {
-		_tprintf(L"Failed to open the main thread!\nExiting...\n");
+		_tprintf(L"Failed to open the main thread!\n");
 		return 0;
 	}
-
-	unsigned long* base_address = NULL;
-	unsigned long* xinput_address = NULL;
-	unsigned long* msvcr_address = NULL;
 
 	// find base addresses and store them
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
 	if(snapshot == INVALID_HANDLE_VALUE) {
-		_tprintf(L"Failed to create snapshot of modules!\nExiting...\n");
+		_tprintf(L"Failed to create snapshot of modules!\n");
 		return 0;
 	}
 	MODULEENTRY32 entry;
 	entry.dwSize = sizeof(MODULEENTRY32);
 	if(Module32First(snapshot, &entry)) {
 		do {
-			if(_tcsicmp(entry.szModule, HAT_EXE_NAME) == 0) base_address = (unsigned long*)entry.modBaseAddr;
-			else if(_tcsicmp(entry.szModule, XINPUT_DLL_NAME) == 0) xinput_address = (unsigned long*)entry.modBaseAddr;
-			else if(_tcsicmp(entry.szModule, MSVCR_DLL_NAME) == 0) msvcr_address = (unsigned long*)entry.modBaseAddr;
+			if(_tcsicmp(entry.szModule, HAT_EXE_NAME) == 0) base_address = entry.modBaseAddr;
+			else if(_tcsicmp(entry.szModule, XINPUT_DLL_NAME) == 0) xinput_address = entry.modBaseAddr;
+			else if(_tcsicmp(entry.szModule, MSVCR_DLL_NAME) == 0) msvcr_address = entry.modBaseAddr;
 			if(base_address != NULL && xinput_address != NULL && msvcr_address != NULL) break;
 		}
 		while(Module32Next(snapshot, &entry));
 	}
 	CloseHandle(snapshot);
 	if(base_address == NULL || xinput_address == NULL || msvcr_address == NULL) {
-		_tprintf(L"Failed to find base address of " HAT_EXE_NAME ", " XINPUT_DLL_NAME ", or " MSVCR_DLL_NAME "!\nExiting...\n");
+		_tprintf(L"Failed to find base address of " HAT_EXE_NAME ", " XINPUT_DLL_NAME ", or " MSVCR_DLL_NAME "!\n");
 		return 0;
 	}
 
@@ -113,17 +110,50 @@ int main(int argc, char* argv[]) {
 	DebugActiveProcess(pid);
 	DebugSetProcessKillOnExit(FALSE); // we want it to keep running at end-of-TAS
 
-	// patch delta
-	WriteProcessMemory(process, base_address + DELTA_WRITE_CODE_OFFSET, nop_delta_buf, sizeof(nop_delta_buf), NULL); // code
-	WriteProcessMemory(process, base_address + DELTA_DATA_OFFSET, &desired_framerate, sizeof(desired_framerate), NULL); // data
-	_tprintf(L"Wrote a fixed delta time of %f!\n", desired_framerate);
-
-	// playing
+	// rand replacement
 	//
 
-	printf(meta.type == IMMEDIATE ? "Playing %s in two seconds!\n" : "Waiting to play %s!\n", meta.name);
-	Sleep(2000); // give user time to give hat focus
+	// allocate new memory
+	new_rand = VirtualAllocEx(process, NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	WriteProcessMemory(process, new_rand, newrand_buf, sizeof(newrand_buf), NULL);
+	_tprintf(L"New rand at 0x%08X!\n", new_rand);
 
+	// make rand code writable
+	VirtualProtectEx(process, msvcr_address + RAND_CODE_OFFSET, 3, PAGE_EXECUTE_READWRITE, &old_rand_prot);
+
+	// save old instruction
+	ReadProcessMemory(process, msvcr_address + RAND_CODE_OFFSET, orig_rand, sizeof(orig_rand), NULL);
+
+	return 1;
+}
+
+BOOL WINAPI cleanup_handler(DWORD signal) {
+	if(signal == CTRL_C_EVENT) {
+		// game memory
+		WriteProcessMemory(process, base_address + TICK_CODE_OFFSET, &orig_tick, sizeof(orig_tick), NULL);
+		WriteProcessMemory(process, base_address + GET_STATE_CODE_OFFSET, &orig_xinput, sizeof(orig_xinput), NULL);
+		WriteProcessMemory(process, base_address + POST_GET_STATE_CODE_OFFSET, &orig_post_xinput, sizeof(orig_post_xinput), NULL);
+		WriteProcessMemory(process, base_address + DELTA_DATA_OFFSET, &original_delta, sizeof(original_delta), NULL); // put back a fixed delta matching the original fps setting
+		if(fps_address != NULL) WriteProcessMemory(process, fps_address, &original_fps, sizeof(original_fps), NULL); // write back original fps value
+
+		// rand
+		WriteProcessMemory(process, msvcr_address + RAND_CODE_OFFSET, orig_rand, sizeof(orig_rand), NULL);
+		VirtualFreeEx(process, new_rand, 0x1000, MEM_RELEASE);
+		VirtualProtectEx(process, msvcr_address + RAND_CODE_OFFSET, 3, old_rand_prot, &old_rand_prot);
+
+		ResumeThread(thread);
+		DebugActiveProcessStop(pid);
+		CloseHandle(thread);
+		CloseHandle(process);
+
+		exit(0);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+void play_movie() {
 	bool started = false;
 	int i = 0;
 	long long xinput_player = -1;
@@ -138,16 +168,11 @@ int main(int argc, char* argv[]) {
 	bool old_timer_paused = false;
 
 	BYTE* xinput_state_address = NULL;
-	BYTE* fps_address = NULL;
 
 	CONTEXT context;
 	context.ContextFlags = CONTEXT_FULL;
 
 	DEBUG_EVENT debug_event;
-
-	// rand
-	void* new_rand = NULL;
-	unsigned long old_rand_prot;
 
 	// main loop
 	while(true) {
@@ -164,21 +189,21 @@ int main(int argc, char* argv[]) {
 			SetThreadContext(thread, &context);
 		}
 		// about to call XInputGetState -- save function arguments
-		else if(started && debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT && debug_event.u.Exception.ExceptionRecord.ExceptionCode == EXCEPTION_BREAKPOINT && debug_event.u.Exception.ExceptionRecord.ExceptionAddress == (BYTE*)base_address + GET_STATE_CODE_OFFSET) {
+		else if(started && debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT && debug_event.u.Exception.ExceptionRecord.ExceptionCode == EXCEPTION_BREAKPOINT && debug_event.u.Exception.ExceptionRecord.ExceptionAddress == base_address + GET_STATE_CODE_OFFSET) {
 			GetThreadContext(thread, &context);
 			context.Rip -= 1;
 			xinput_player = context.Rcx;
 			xinput_state_address = (BYTE*)context.Rdx;
 			SetThreadContext(thread, &context);
 
-			WriteProcessMemory(process, (BYTE*)base_address + POST_GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL);
-			WriteProcessMemory(process, (BYTE*)base_address + GET_STATE_CODE_OFFSET, &orig_xinput, sizeof(orig_xinput), NULL);
+			WriteProcessMemory(process, base_address + POST_GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL);
+			WriteProcessMemory(process, base_address + GET_STATE_CODE_OFFSET, &orig_xinput, sizeof(orig_xinput), NULL);
 			ContinueDebugEvent(debug_event.dwProcessId, debug_event.dwThreadId, DBG_CONTINUE);
 			ResumeThread(thread);
 			continue;
 		}
 		// finished calling XInputGetState -- overwrite output with correct player input as specified in the TAS
-		else if(started && debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT && debug_event.u.Exception.ExceptionRecord.ExceptionCode == EXCEPTION_BREAKPOINT && debug_event.u.Exception.ExceptionRecord.ExceptionAddress == (BYTE*)base_address + POST_GET_STATE_CODE_OFFSET) {
+		else if(started && debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT && debug_event.u.Exception.ExceptionRecord.ExceptionCode == EXCEPTION_BREAKPOINT && debug_event.u.Exception.ExceptionRecord.ExceptionAddress == base_address + POST_GET_STATE_CODE_OFFSET) {
 			GetThreadContext(thread, &context);
 			context.Rip -= 1;
 			SetThreadContext(thread, &context);
@@ -186,8 +211,8 @@ int main(int argc, char* argv[]) {
 			// write inputs
 			WriteProcessMemory(process, xinput_state_address + 4, &reports[i].gamepads[xinput_player], sizeof(state_report), NULL);
 
-			WriteProcessMemory(process, (BYTE*)base_address + GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL);
-			WriteProcessMemory(process, (BYTE*)base_address + POST_GET_STATE_CODE_OFFSET, &orig_post_xinput, sizeof(orig_post_xinput), NULL);
+			WriteProcessMemory(process, base_address + GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL);
+			WriteProcessMemory(process, base_address + POST_GET_STATE_CODE_OFFSET, &orig_post_xinput, sizeof(orig_post_xinput), NULL);
 			ContinueDebugEvent(debug_event.dwProcessId, debug_event.dwThreadId, DBG_CONTINUE);
 			ResumeThread(thread);
 			continue;
@@ -201,20 +226,34 @@ int main(int argc, char* argv[]) {
 		/*
 		** main breakpoint hit!
 		*/
-		
+
 		old_timer_paused = timer_paused; // like in ASL
-		ReadProcessMemory(process, base_address + TIMER_OFFSET + (0x10 / 4), &timer_paused, sizeof(timer_paused), NULL); // read timer status
+		ReadProcessMemory(process, base_address + TIMER_OFFSET + 0x10, &timer_paused, sizeof(timer_paused), NULL); // read timer status
 		old_game_time = game_time; // like in ASL
-		ReadProcessMemory(process, base_address + TIMER_OFFSET + (0x34 / 4), &game_time, sizeof(game_time), NULL); // read game time
+		ReadProcessMemory(process, base_address + TIMER_OFFSET + 0x34, &game_time, sizeof(game_time), NULL); // read game time
 		old_act_time = act_time; // like in ASL
-		ReadProcessMemory(process, base_address + TIMER_OFFSET + (0x3C / 4), &act_time, sizeof(act_time), NULL); // read act time
-	
+		ReadProcessMemory(process, base_address + TIMER_OFFSET + 0x3C, &act_time, sizeof(act_time), NULL); // read act time
+
 		// start if fullgame type, and game timer just started/game timer just resumed, OR start if IL type, and act timer just started from zero, OR start immediately if type is immediate
 		// !started in the beginning should make it not evaluate all the conditions once TAS has started
 		if(!started && (meta.type == FULLGAME && game_time > 0 && (old_game_time == 0 || (!timer_paused && old_timer_paused)) || (meta.type == INDIVIDUAL && act_time > 0 && old_act_time == 0) || (meta.type == IMMEDIATE))) {
 			// write the input breakpoint on starting
-			WriteProcessMemory(process, (BYTE*)base_address + GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL); // code
-			WriteProcessMemory(process, (BYTE*)base_address + POST_GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL); // code
+			WriteProcessMemory(process, base_address + GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL); // code
+			WriteProcessMemory(process, base_address + POST_GET_STATE_CODE_OFFSET, &interrupt_op, sizeof(interrupt_op), NULL); // code
+
+			printf("Beginning, parsing %s... ", movie_filename);
+			reports = parse_tas(movie_filename, &meta);
+			_tprintf(L"done!\n");
+			desired_framerate = 1.f / meta.fps;
+
+			if(meta.type == IMMEDIATE) {
+				_tprintf(L"Starting in two seconds...\n");
+				Sleep(2000);
+			}
+
+			// patch delta
+			WriteProcessMemory(process, base_address + DELTA_WRITE_CODE_OFFSET, nop_delta_buf, sizeof(nop_delta_buf), NULL); // code
+			WriteProcessMemory(process, base_address + DELTA_DATA_OFFSET, &desired_framerate, sizeof(desired_framerate), NULL); // data
 
 			started_time = game_time - desired_framerate; // for giving a duration at the end message, subtract a frame, as start time gets set a frame into the replay
 
@@ -226,30 +265,22 @@ int main(int argc, char* argv[]) {
 			// write the FPS value that'll be used for the TAS
 			WriteProcessMemory(process, fps_address, &meta.fps, sizeof(meta.fps), NULL);
 
-			// rand replacement
-			//
-
-			// allocate new memory
-			new_rand = VirtualAllocEx(process, NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-			WriteProcessMemory(process, new_rand, newrand_buf, sizeof(newrand_buf), NULL);
-			_tprintf(L"New rand at 0x%08X!\n\n", new_rand);
-
-			// make rand code writable
-			VirtualProtectEx(process, (BYTE*)msvcr_address + RAND_CODE_OFFSET, 3, PAGE_EXECUTE_READWRITE, &old_rand_prot);
-
-			// save old instruction
-			ReadProcessMemory(process, (BYTE*)msvcr_address + RAND_CODE_OFFSET, orig_rand, sizeof(orig_rand), NULL);
+			// reset rng
+			// sequence length
+			WriteProcessMemory(process, (BYTE*)new_rand + 0x1F8, &zero, sizeof(zero), NULL);
+			// sequence index
+			WriteProcessMemory(process, (BYTE*)new_rand + 0x1FC, &zero, sizeof(zero), NULL);
+			// sequence
+			WriteProcessMemory(process, (BYTE*)new_rand + 0x200, &zero, sizeof(zero), NULL);
 
 			// patch to jump to new rand
-			int jump_target = (BYTE*)new_rand - ((BYTE*)msvcr_address + RAND_CODE_OFFSET) - 5; // 5 is the length of the jmp instruction
-			WriteProcessMemory(process, (BYTE*)msvcr_address + RAND_CODE_OFFSET, &jmp_op, sizeof(jmp_op), NULL);
-			WriteProcessMemory(process, (BYTE*)msvcr_address + RAND_CODE_OFFSET + 1, &jump_target, sizeof(jump_target), NULL);
+			int jump_target = (BYTE*)new_rand - (msvcr_address + RAND_CODE_OFFSET) - 5; // 5 is the length of the jmp instruction
+			WriteProcessMemory(process, msvcr_address + RAND_CODE_OFFSET, &jmp_op, sizeof(jmp_op), NULL);
+			WriteProcessMemory(process, msvcr_address + RAND_CODE_OFFSET + 1, &jump_target, sizeof(jump_target), NULL);
 
 			// set start flag
 			started = true;
-			if(meta.type != IMMEDIATE) {
-				printf("Starting playback!\n");
-			}
+			_tprintf(L"Starting playback!\n");
 		}
 
 		// restart loop if it hasn't started yet
@@ -295,23 +326,64 @@ int main(int argc, char* argv[]) {
 	// end main loop
 
 	// cleanup
+	WriteProcessMemory(process, msvcr_address + RAND_CODE_OFFSET, orig_rand, sizeof(orig_rand), NULL);
+	WriteProcessMemory(process, base_address + GET_STATE_CODE_OFFSET, &orig_xinput, sizeof(orig_xinput), NULL);
+	WriteProcessMemory(process, fps_address, &original_fps, sizeof(original_fps), NULL);
+
+	_tprintf(L"Done in %f!\n\n", meta.type == INDIVIDUAL ? act_time : game_time - started_time);
+}
+
+int main(int argc, char* argv[]) {
+	bool repeat = false;
+
+	for(int i = 1; i < argc; i++) {
+		if(argv[i][0] == '-') {
+			switch(argv[i][1]) {
+				case 'r': repeat = true;
+			}
+		}
+		else movie_filename = argv[i];
+	}
+
+	if(movie_filename == NULL) {
+		_tprintf(L"A TAS movie filename wasn't specified!\nUsage: hatTAS.exe [-r] <movie.htas>\n\nExiting...\n");
+		return 0;
+	}
+
+	if(!init()) {
+		_tprintf(L"Failed initializing...\nExiting...\n");
+		return 0;
+	}
+
+	// catch ctrl+c for cleanup reasons
+	SetConsoleCtrlHandler(cleanup_handler, TRUE);
+
+	_tprintf(L"Done initializing, ready!\n\n");
+
+	do {
+		play_movie();
+	}
+	while(repeat);
+
+	// cleanup
 	//
 
 	// game memory
-	WriteProcessMemory(process, (BYTE*)base_address + GET_STATE_CODE_OFFSET, &orig_xinput, sizeof(orig_xinput), NULL);
-	WriteProcessMemory(process, (BYTE*)base_address + POST_GET_STATE_CODE_OFFSET, &orig_post_xinput, sizeof(orig_post_xinput), NULL);
+	WriteProcessMemory(process, base_address + TICK_CODE_OFFSET, &orig_tick, sizeof(orig_tick), NULL);
+	WriteProcessMemory(process, base_address + GET_STATE_CODE_OFFSET, &orig_xinput, sizeof(orig_xinput), NULL);
+	WriteProcessMemory(process, base_address + POST_GET_STATE_CODE_OFFSET, &orig_post_xinput, sizeof(orig_post_xinput), NULL);
 	WriteProcessMemory(process, base_address + DELTA_DATA_OFFSET, &original_delta, sizeof(original_delta), NULL); // put back a fixed delta matching the original fps setting
-	WriteProcessMemory(process, fps_address, &original_fps, sizeof(original_fps), NULL); // write back original fps value
+	if(fps_address != NULL) WriteProcessMemory(process, fps_address, &original_fps, sizeof(original_fps), NULL); // write back original fps value
 
 	// rand
-	WriteProcessMemory(process, (BYTE*)msvcr_address + RAND_CODE_OFFSET, orig_rand, sizeof(orig_rand), NULL);
+	WriteProcessMemory(process, msvcr_address + RAND_CODE_OFFSET, orig_rand, sizeof(orig_rand), NULL);
 	VirtualFreeEx(process, new_rand, 0x1000, MEM_RELEASE);
-	VirtualProtectEx(process, (BYTE*)msvcr_address + RAND_CODE_OFFSET, 3, old_rand_prot, &old_rand_prot);
+	VirtualProtectEx(process, msvcr_address + RAND_CODE_OFFSET, 3, old_rand_prot, &old_rand_prot);
 
+	ResumeThread(thread);
 	DebugActiveProcessStop(pid);
 	CloseHandle(thread);
 	CloseHandle(process);
-	_tprintf(L"Done in %lf!\n", meta.type == INDIVIDUAL ? act_time : game_time - started_time);
 
 	return 0;
 }
